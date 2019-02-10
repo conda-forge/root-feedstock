6f190a0156ae53ac4b83827eb041f2bb53909080
Christopher Burr <christopher.burr@cern.ch>
2019-02-10 14:59:55 +0000

    Improve warning message if CONDA_BUILD_SYSROOT is not set

--- recipe/activate.fish
+++ recipe/activate.fish
@@ -7,8 +7,11 @@ switch (uname)
     # Only if not in the base env (let's be nice)
     case Darwin
         if [ "$CONDA_DEFAULT_ENV" != "base" ]
-            if not set -q CONDA_BUILD_SYSROOT ; and not set -q SDKROOT
-                echo "You will need CONDA_BUILD_SYSROOT or SDKROOT to compile with cling, download the macOS 10.9 SDK"
+            if not set -q CONDA_BUILD_SYSROOT
+                echo "WARNING: Compiling likely won't work unless you: download the macOS 10.9 SDK, set CONDA_BUILD_SYSROOT and reactivate the environment."
+                echo "You can probably ignore this warning and just omit + or ++ when executing ROOT macros."
+            else
+                export SDKROOT="$CONDA_BUILD_SYSROOT"
             end
         end
 end
--- recipe/activate.sh
+++ recipe/activate.sh
@@ -4,12 +4,13 @@ export ROOTSYS="${CONDA_PREFIX}"
 
 # Only on macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-
     # Only if not in the base env (let's be nice)
-    if [ "${CONDA_DEFAULT_ENV}"  == "base" ] ; then
-        if [ -z "${CONDA_BUILD_SYSROOT}" ]  && [ -z "${SDKROOT}" ] ; then
-            echo "You will need CONDA_BUILD_SYSROOT or SDKROOT to compile with cling, download the macOS 10.9 SDK"
+    if [ "${CONDA_DEFAULT_ENV}"  != "base" ] ; then
+        if [ -z "${CONDA_BUILD_SYSROOT}" ] ; then
+            echo "WARNING: Compiling likely won't work unless you: download the macOS 10.9 SDK, set CONDA_BUILD_SYSROOT and reactivate the environment."
+            echo "You can probably ignore this warning and just omit + or ++ when executing ROOT macros."
+        else
+            export SDKROOT="${CONDA_BUILD_SYSROOT}"
         fi
     fi
 fi
-
