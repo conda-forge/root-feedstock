2139f194978d1102b62f3283802a0b49e8f753ab
Chris Burr <chrisburr@users.noreply.github.com>
2020-06-15 19:11:37 +0200

    Fix skipping test for Python 3.6

--- recipe/test_pyroot.py
+++ recipe/test_pyroot.py
@@ -12,6 +12,6 @@ assert fn(6) == 36
 
 # ROOT doesn't like how conda-forge's Python 3.6 binaries are linked
 # This is expected to fail but it should be fixed upstream rather than patched here
-if sys.version_info > (3, 6):
+if sys.version_info >= (3, 7):
     assert ROOT.TPython.Exec("print(1+1)")
     assert int(ROOT.TPython.Eval("1+1")) == 2
