3a90fb5b6c43574c458f496ade2c3ac72a903c92
Duncan Macleod <duncan.macleod@ligo.org>
2021-10-05 15:35:02 +0100

    preserve external ROOTSYS in activate/deactivate cycle
    (cherry picked from commit 9a007eee343db0a7eaa779237f9101bc59e75686)

--- recipe/activate.fish
+++ recipe/activate.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
+if set -q ROOTSYS
+	set -gx CONDA_BACKUP_ROOTSYS "$ROOTSYS"
+end
+
 set -gx ROOTSYS "$CONDA_PREFIX"
--- recipe/deactivate.sh
+++ recipe/deactivate.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-unset ROOTSYS
+# reinstate the backup from outside the environment
+if [ ! -z "${CONDA_BACKUP_ROOTSYS}" ]; then
+	export ROOTSYS="${CONDA_BACKUP_ROOTSYS}"
+	unset CONDA_BACKUP_ROOTSYS
+# no backup, just unset
+else
+	unset ROOTSYS
+fi
