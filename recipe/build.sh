3be4972770ca637dbe21df82dcdd953233c4e664
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2024-06-18 13:56:35 +0200

    Various fixes for MacOS builds
    * Use default provider value for osx_arm64
    
    As described at https://conda-forge.org/docs/maintainer/conda_forge_yml/#provider
    
    * Automatically detect correct clang version in build.sh
    
    * Force one modulemap for Darwin
    
    * Remove checks for SDK versions in cling
    
    * Avoid link TPython against libPython on MacOS
    
    Use the correct CMake target `Python::Module` in the TPython CMakeLists.txt. This will create a libROOTTPython shared library that does not link on MacOS. In a Python environment, TPython will work, but it will not work from C++. This can only work if the library is linked against libpython. To represent the same situation as before this commit, the patch is applied only on MacOS. In fact currently the conda build links against libpython on Linux but on MacOS.

--- recipe/build.sh
+++ recipe/build.sh
@@ -28,7 +28,7 @@ declare -a CMAKE_PLATFORM_FLAGS
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
     CONDA_SUBDIR=${target_platform} conda create --prefix "${SRC_DIR}/clang_env" --yes \
-        "llvm 13.0.1" "clangdev 13.0.1 ${clang_patches_version}*"
+        "llvm ${clang_version}" "clangdev ${clang_version} ${clang_patches_version}*"
     Clang_DIR=${SRC_DIR}/clang_env
     CMAKE_PLATFORM_FLAGS+=("-DLLVM_CMAKE_PATH=${SRC_DIR}/clang_env/lib/cmake")
 else
@@ -53,7 +53,8 @@ if [[ "${target_platform}" == linux* ]]; then
     echo "CXXFLAGS is now '${CXXFLAGS}'"
 else
     CMAKE_PLATFORM_FLAGS+=("-DBLA_PREFER_PKGCONFIG=ON")
-    CMAKE_PLATFORM_FLAGS+=("-DCLANG_RESOURCE_DIR_VERSION=13.0.1")
+    clang_version_split=(${clang_version//./ })
+    CMAKE_PLATFORM_FLAGS+=("-DCLANG_RESOURCE_DIR_VERSION=${clang_version_split[0]}")
 
     # HACK: Hack the macOS SDK to make rootcling find the correct ncurses
     if [[ -f  "$CONDA_BUILD_SYSROOT/usr/include/module.modulemap.bak" ]]; then
