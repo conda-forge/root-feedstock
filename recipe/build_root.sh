f7016644b715cf203aaf3450120c086256e874d4
Chris Burr <christopher.burr@cern.ch>
2025-01-26 23:17:49 +0100

    Try to fix cross-compiling macOS

--- recipe/build_root.sh
+++ recipe/build_root.sh
@@ -38,12 +38,12 @@ sed -i -e "s@${OLDVERSIONMACOS}@${MACOSX_DEPLOYMENT_TARGET}@g" \
 declare -a CMAKE_PLATFORM_FLAGS
 
 if [[ "${target_platform}" != "${build_platform}" && "${target_platform}" == osx* ]]; then
-    CONDA_SUBDIR=${target_platform} micromamba create --prefix "${SRC_DIR}/clang_env" --yes \
+    CONDA_SUBDIR=${target_platform} conda create --prefix "${SRC_DIR}/clang_env" --yes \
         "llvm ${clang_version}" "clangdev ${clang_version} ${clang_patches_version}*"
     Clang_DIR=${SRC_DIR}/clang_env
     CMAKE_PLATFORM_FLAGS+=("-DLLVM_CMAKE_PATH=${SRC_DIR}/clang_env/lib/cmake")
 
-    CONDA_SUBDIR=${build_platform} micromamba create --prefix "${SRC_DIR}/clang_env_build" --yes \
+    CONDA_SUBDIR=${build_platform} conda create --prefix "${SRC_DIR}/clang_env_build" --yes \
         "llvm ${clang_version}" "clangdev ${clang_version} ${clang_patches_version}*"
     Clang_DIR_BUILD=${SRC_DIR}/clang_env_build
 else
