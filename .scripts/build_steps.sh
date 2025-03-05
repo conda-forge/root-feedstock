38fa87031d0183554448ba96045f223757b75e09
Chris Burr <christopher.burr@cern.ch>
2025-03-06 00:13:28 +0100

    Enable test: native_and_emulated

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -60,9 +60,10 @@ ulimit -n 1024
 # make the build number clobber
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
-if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]] && [[ "${BUILD_WITH_CONDA_DEBUG:-0}" != 1 ]]; then
-    EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --no-test"
+if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]] && [[ "${HOST_PLATFORM}" != linux-* ]] && [[ "${BUILD_WITH_CONDA_DEBUG:-0}" != 1 ]]; then
+    EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --test skip"
 fi
+
 
 ( endgroup "Configuring conda" ) 2> /dev/null
 
@@ -89,7 +90,6 @@ else
     ( endgroup "Inspecting artifacts" ) 2> /dev/null
     ( startgroup "Validating outputs" ) 2> /dev/null
 
-    sed -i.bak 's@m.config.variant = package_variants\[0\]@m.config.variant = package_variants[0] if package_variants else {}@g' $(python -c 'import rattler_build_conda_compat.render; print(rattler_build_conda_compat.render.__file__)')
     validate_recipe_outputs "${FEEDSTOCK_NAME}"
 
     ( endgroup "Validating outputs" ) 2> /dev/null
