77329a3034c2eb2c84bf8f167f3092c3e6272504
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2025-08-28 14:32:17 +0200

    Further changes to fix MacOS builds
    * Remove custom clang and llvm installations in conda ${SRC_DIR}, instead rely on declaring the correct `build` and `host` dependencies on the MacOS and cross-compilation builds
    * Ensure the build does not link against LLVM libraries
    * Specify compiler version on MacOS to ensure the shipped libc++ is compatible with the one expected by cling (i.e. by our patched versions of clang/LLVM)

--- recipe/build_root.sh
+++ recipe/build_root.sh
@@ -37,26 +37,8 @@ sed -i -e "s@${OLDVERSIONMACOS}@${MACOSX_DEPLOYMENT_TARGET}@g" \
 
 declare -a CMAKE_PLATFORM_FLAGS
 
-if [[ "${target_platform}" == osx-arm64 ]]; then
-    CONDA_SUBDIR=${target_platform} conda create --prefix "${SRC_DIR}/clang_env" --yes \
-        "llvm ${clang_version} ${clang_patches_version}*" \
-        "llvm-openmp ${clang_version}" \
-        "clangdev ${clang_version} ${clang_patches_version}*"
-    Clang_DIR=${SRC_DIR}/clang_env
-    CMAKE_PLATFORM_FLAGS+=("-DLLVM_CMAKE_PATH=${SRC_DIR}/clang_env/lib/cmake")
-else
-    Clang_DIR=${PREFIX}
-fi
-
-if [[ "${target_platform}" != "${build_platform}" && "${target_platform}" == osx* ]]; then
-    CONDA_SUBDIR=${build_platform} conda create --prefix "${SRC_DIR}/clang_env_build" --yes \
-        "llvm ${clang_version} ${clang_patches_version}*" \
-        "llvm-openmp ${clang_version}" \
-        "clangdev ${clang_version} ${clang_patches_version}*"
-    Clang_DIR_BUILD=${SRC_DIR}/clang_env_build
-else
-    Clang_DIR_BUILD=${BUILD_PREFIX}
-fi
+Clang_DIR=${PREFIX}
+Clang_DIR_BUILD=${BUILD_PREFIX}
 
 clang_version_split=(${clang_version//./ })
 CMAKE_PLATFORM_FLAGS+=("-DCLANG_RESOURCE_DIR_VERSION=${clang_version_split[0]}")
@@ -188,7 +170,13 @@ else
     CMAKE_PLATFORM_FLAGS+=("-Dclad=ON")
 
     # Cling needs some minor patches to the LLVM sources, hackily apply them rather than rebuilding LLVM
+    # The following lines are used to avoid linking against LLVM during the build of ROOT, which is not supported.
+    # The setting is done globally by the LLVM CMake configuration and it overrides any other setting in our build
+    # For more details, see https://github.com/root-project/root/issues/18387 and https://github.com/llvm/llvm-project/pull/135570
     sed -i "s@LLVM_LINK_LLVM_DYLIB yes@LLVM_LINK_LLVM_DYLIB no@g" "${Clang_DIR}/lib/cmake/llvm/LLVMConfig.cmake"
+    if [[ "${target_platform}" != "${build_platform}" ]]; then
+        sed -i "s@LLVM_LINK_LLVM_DYLIB yes@LLVM_LINK_LLVM_DYLIB no@g" "${Clang_DIR_BUILD}/lib/cmake/llvm/LLVMConfig.cmake"
+    fi
 fi
 
 # Enable some vectorisation options
@@ -232,7 +220,7 @@ if [[ "${target_platform}" != "${build_platform}" ]]; then
     CONDA_BUILD_SYSROOT_BUILD=$CONDA_BUILD_SYSROOT
 
     if [[ "${target_platform}" == osx* ]]; then
-        CMAKE_PLATFORM_FLAGS_BUILD+=("-DLLVM_CMAKE_PATH=${SRC_DIR}/clang_env_build/lib/cmake")
+        CMAKE_PLATFORM_FLAGS_BUILD+=("-DLLVM_CMAKE_PATH=${Clang_DIR_BUILD}/lib/cmake")
         clang_version_split=(${clang_version//./ })
         CMAKE_PLATFORM_FLAGS_BUILD+=("-DCLANG_RESOURCE_DIR_VERSION=${clang_version_split[0]}")
     elif [[ "${target_platform}" == linux* ]]; then
